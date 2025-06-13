2ee1ef38bc40a18e4c336374c1a1654d3cc6de87
kahqgc <kahqgc@gmail.com>
2025-06-13 08:13:06 -0500

    formatting errors JSON

--- project-task.js
+++ project-task.js
@@ -43,31 +43,29 @@ but it contains multiple syntax errors that prevent it from being parsed.
 // ============================================
 // 🔍 Provided JSON File with Errors
 // ============================================
-
-const invalidBookingJSON = `
+//got rid of variable declaration and backticks - not valid in JSON format
 {
   "hotelName": "Grand City Hotel",
-  "checkInDate": "2024-05-15"
-  "checkOutDate": "2024-05-20",
+  "checkInDate": "2024-05-15",
+  "checkOutDate": "2024-05-20", // invalid syntax: added comma
   "guests": [
     {
-      name: "Alice Johnson",
+      "name": "Alice Johnson", //invalid syntax: put name in ""
       "age": 30,
       "email": "alice.johnson@example.com"
     },
     {
       "name": "Bob Smith",
-      "age": undefined,
+      "age": null, //invalid syntax: switched undefined to null
       "email": "bob.smith@example"
     }
   ],
   "roomDetails": {
     "type": "Suite",
     "pricePerNight": 200,
-    "amenities": ["WiFi", "Breakfast", "Parking",]
+    "amenities": ["WiFi", "Breakfast", "Parking"]//invalid syntax: deleted trailing comma
   }
-}
-`;
+}// invalid syntax: deleted semicolon
 
 
 // ============================================
@@ -91,11 +89,16 @@ const invalidBookingJSON = `
 💬 Reflect and answer the following:
 
 1️⃣ What tools or techniques did you use to identify the errors?
+JSONlint.com
+
 
 2️⃣ How did you confirm that your corrected JSON file was valid?
+As I worked through the errors, I kept fixing them until the site said: JSON valid!
 
 3️⃣ Which errors were the most difficult to spot? Why?
+Errors with brackets. It can get confusing with a lot of nesting what brackets belong to what object or array.
 
 4️⃣ What strategies can help you avoid these kinds of errors in the future?
    (e.g., syntax highlighting, linters, writing JSON by example)
+   using JSONlint.com and installing eslint through vscode to troubleshoot directly on the file.
 */
